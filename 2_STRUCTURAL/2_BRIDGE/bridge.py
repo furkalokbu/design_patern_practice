f4ac704720fa48a15c0c708e013ab820ecfa9c62
Serhii <serhii.furkalo@keyua.org>
2023-05-11 17:29:57 +0300

    add composite

--- 2_STRUCTURAL/2_BRIDGE/bridge.py
+++ 2_STRUCTURAL/2_BRIDGE/bridge.py
@@ -14,3 +14,31 @@ class RasterRenderer(Renderer):
     def render_circle(self, radius):
         print(f'Draving pixels for a circle of radius {radius}')
 
+class Shape:
+    def __init__(self, renderer):
+        self.renderer = renderer
+
+    def draw(self):
+        pass
+
+    def resize(self, factor):
+        pass
+
+class Circle(Shape):
+    def __init__(self, renderer, radius):
+        super().__init__(renderer)
+        self.radius = radius
+
+    def draw(self):
+        self.renderer.render_circle(self.radius)
+
+    def resize(self, factor):
+        self.radius *= factor
+
+if __name__ == '__main__':
+    vector = VectorRenderer()
+    raster = RasterRenderer()
+    circle = Circle(vector, 5)
+    circle.draw()
+    circle.resize(2)
+    circle.draw()
